11e49e828b16c89b2c64229a8ea891ac997f1426
Noe Lutz <noelutz@google.com>
2017-10-11 15:50:31 -0700

    Testing 0.2 recipes

---
+++ 0.2/Testing/source/ShowThings.js
@@ -0,0 +1,39 @@
+// @license
+// Copyright (c) 2017 Google Inc. All rights reserved.
+// This code may only be used under the BSD style license found at
+// http://polymer.github.io/LICENSE.txt
+// Code distributed by Google as part of this project is also
+// subject to an additional IP rights grant found at
+// http://polymer.github.io/PATENTS.txt
+
+"use strict";
+
+defineParticle(({DomParticle}) => {
+
+  let template = `
+<x-list items="{{things}}">
+    <template>
+    <div>
+      <ul>
+        <li>{{name}}</li>
+      </ul>
+    </div>
+    </template>
+</x-list>
+    `.trim();
+
+  return class extends DomParticle {
+    get template() {
+      return template;
+    }
+    _render(props, state) {
+      if (props.things) {
+        return {
+          things: props.things.map(thing => {
+            return { name: thing.name };
+          })
+        };
+      }
+    }
+  };
+});
